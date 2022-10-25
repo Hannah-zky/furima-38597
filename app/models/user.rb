35d248e8d21da36f49d9ed4de54d9740f194ab6e
Hannah-zky <zkyharuna.tech@gmail.com>
2022-10-25 14:31:50 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, :date_of_birth
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+  end
+  
+  VARID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.i
+  validates :password, format: { with: VARID_PASSWORD_REGEX }
+  
 end
