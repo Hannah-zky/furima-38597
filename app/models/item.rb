4857f58e2ee6aac0431a33fb63cc4037feeaf3ea
Hannah-zky <zkyharuna.tech@gmail.com>
2022-10-26 19:22:10 +0900

    Active Hashの導入と設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_date
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name, :price
+    validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :delivery_date_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
 end
