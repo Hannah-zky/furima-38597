52f9964c7726fae3a0d4bcf2e396f9c3965e8fb9
Hannah-zky <zkyharuna.tech@gmail.com>
2022-10-28 16:39:36 +0900

    バリデーションの再修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,6 @@ class Item < ApplicationRecord
 validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :delivery_date_id, numericality: { other_than: 1, message: "can't be blank" }
   with_options presence: true do
     validates :image, :name, :explanation
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 end
