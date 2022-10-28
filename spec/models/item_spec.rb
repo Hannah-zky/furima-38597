3859a3f90291cec4db2278e15f20a956c969f2f2
Hannah-zky <zkyharuna.tech@gmail.com>
2022-10-28 16:41:12 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -53,6 +53,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery date can't be blank")
       end
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
       it 'priceが299以下だと出品できない' do
         @item.price = 299
         @item.valid?
@@ -67,6 +72,11 @@ RSpec.describe Item, type: :model do
         @item.price = "１００００"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
